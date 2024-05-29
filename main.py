8172f3e081a8bf0e12ce33c1a5c18cce4f82f990
Vincent van Vliet <b.v.vanvliet@student.tudelft.nl>
2024-05-29 14:19:28 +0200

    Seperate data_frames into different data types

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import pandas as pd
 
 def main():
     # Read data
-    data_frames = retrieve_data()
+    wer_data_frames, cer_data_frames = retrieve_data()
 
-    # Process the data based on specific metric, input_type and speech_type
-    process_data_per_speaker_group(data_frames, metric='median', input_type='NoAug', speech_type='Read')
-    process_data_per_speech_type(data_frames, metric='median', input_type='NoAug', speech_type='Read')
+    # Process the data based on specific metric, input_type, and speech_type
+    process_data_per_speaker_group(wer_data_frames, metric='std', input_type='NoAug', speech_type='Read', data_type='WER')
+    process_data_per_speaker_group(cer_data_frames, metric='std', input_type='NoAug', speech_type='Read', data_type='CER')
+
+    # Process data per data_type, per speech type
+    process_data_per_speech_type(wer_data_frames, metric='std', input_type='NoAug', speech_type='Read', data_type='WER')
+    process_data_per_speech_type(cer_data_frames, metric='std', input_type='NoAug', speech_type='Read', data_type='CER')
 
 
 def read_file(file_path):
@@ -27,7 +31,8 @@ def retrieve_data():
     files_hmi = ['DC_Hmi_{parent}', 'DOA_Hmi_{parent}', 'DT_Hmi_{parent}', 'NnA_Hmi_{parent}', 'NnT_Hmi_{parent}']
 
     # Read files into dataframes
-    data_frames = {}
+    wer_data_frames = {}
+    cer_data_frames = {}
 
     for parent in parent_folders:
         for child in child_folders:
@@ -40,16 +45,20 @@ def retrieve_data():
 
                     try:
                         df = read_file(file_path)
-                        data_frames[file_path] = df
+                        if parent == 'WER':
+                            wer_data_frames[file_path] = df
+                        else:
+                            cer_data_frames[file_path] = df
                     except Exception as e:
                         print(f'Failed to read {file_path}: {e}')
 
-    return data_frames
+    return wer_data_frames, cer_data_frames
 
 
-def process_data_per_speaker_group(data_frames, metric='median', input_type='NoAug', speech_type='Read'):
+def process_data_per_speaker_group(data_frames, metric='median', input_type='NoAug', speech_type='Read', data_type='WER'):
+    # Get the pandas function for the specified metric
+    metric_func = getattr(pd.Series, metric)
     values = {}
-    metric_func = getattr(pd.Series, metric)  # Get the pandas function for the specified metric
 
     speech_type_keywords = {
         'Read': ['DC_Read_', 'DOA_Read_', 'DT_Read_', 'NnA_Read_', 'NnT_Read_'],
@@ -67,17 +76,18 @@ def process_data_per_speaker_group(data_frames, metric='median', input_type='NoA
             metric_value = metric_func(df['Value'])
             values[file_path] = metric_value
 
-    # Print values
+    # Write to output file
     output = ''
     for file_path, metric_value in values.items():
-        output += f'{metric.capitalize()} value for {file_path}: {metric_value}'
-        print(output)
+        output_string = f'{metric.capitalize()} value for {file_path}: {metric_value}\n'
+        print(output_string)
+        output += output_string
 
-    # Write to output file
-    with open(f'{metric}-{input_type}-{speech_type}.txt', 'w') as file:
-        file.write(output + '\n')
+    with open(f'results/{metric}-{input_type}-{speech_type}-{data_type}.txt', 'w') as file:
+        file.write(output)
 
-def process_data_per_speech_type(data_frames, metric='median', input_type='NoAug', speech_type='Read'):
+
+def process_data_per_speech_type(data_frames, metric='median', input_type='NoAug', speech_type='Read', data_type='WER'):
     # Get the pandas function for the specified metric
     metric_func = getattr(pd.Series, metric)
 
@@ -87,23 +97,24 @@ def process_data_per_speech_type(data_frames, metric='median', input_type='NoAug
         'Hmi': ['DC_Hmi_', 'DOA_Hmi_', 'DT_Hmi_', 'NnA_Hmi_', 'NnT_Hmi_']
     }
 
+    # Check for valid speech type
     if speech_type not in speech_type_keywords:
         print(f"Invalid speech_type: {speech_type}. Valid options are 'Read' or 'Hmi'.")
         return
 
     # Combine values for each speaker group
     combined_values = []
-
     for file_path, df in data_frames.items():
         if input_type in file_path and any(keyword in file_path for keyword in speech_type_keywords[speech_type]):
             combined_values.append(df['Value'])
 
     if combined_values:
+        # Concatenate values and apply the metric function
         combined_series = pd.concat(combined_values, ignore_index=True)
         overall_metric_value = metric_func(combined_series)
-        output = f'Overall {metric.capitalize()} value for {input_type} {speech_type} data: {overall_metric_value}'
+        output = f'Overall {metric.capitalize()} value for {input_type} {speech_type} {data_type} data: {overall_metric_value}'
         print(output)
-        with open(f'overall-{metric}-{input_type}-{speech_type}.txt', 'w') as file:
+        with open(f'results/overall-{metric}-{input_type}-{speech_type}-{data_type}.txt', 'w') as file:
             file.write(output + '\n')
     else:
         print(f'No data found for {input_type} {speech_type}.')
